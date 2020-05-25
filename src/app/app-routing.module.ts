9915af173e7f1cc3cc999ae3950b4480a6af9ac2
mh <objectifmh@hotmail.com>
2020-05-25 04:07:03 +0200

    deploy with onSameUrlNavigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,22 @@ import { FormValidationComponent } from './Inscription/form-validation.component
 import { ListeTendanceSlideComponent } from './liste-tendance-slide/liste-tendance-slide.component';
 import { ListeRechercheFilmComponent } from './liste-recherche-film/liste-recherche-film.component';
 import { PeopleComponent } from './people/people.component';
- 
+
 const routes: Routes = [
   {path: 'home', component: ListeTendanceSlideComponent},
   {path: 'inscription', component: FormValidationComponent},
   {path: 'recherche', component: ListeRechercheFilmComponent},
   {path: 'people', component: PeopleComponent},
   {path: 'people/:id', component: PeopleComponent},
+  { path: '**', redirectTo: '/home' },
 
   /* {path: '**', redirectTo: '/' } */
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
